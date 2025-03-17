d1a19f4631ddcf3e2475a33b2f1b7f779057643f
Pechy83 <danik.pechy1983@gmail.com>
2025-03-17 10:46:54 +0100

    aktualizace layout a foto

--- docs/static/js/scripts.js
+++ docs/static/js/scripts.js
@@ -10,33 +10,28 @@
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
-    var navbarShrink = function () {
+    const navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
         if (!navbarCollapsible) {
             return;
         }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
-
+        navbarCollapsible.classList.toggle('navbar-shrink', window.scrollY !== 0);
     };
 
-    // Shrink the navbar 
+    // Shrink the navbar on load
     navbarShrink();
 
-    // Shrink the navbar when page is scrolled
+    // Shrink the navbar on scroll
     document.addEventListener('scroll', navbarShrink);
 
-    //  Activate Bootstrap scrollspy on the main nav element
+    // Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
         new bootstrap.ScrollSpy(document.body, {
             target: '#mainNav',
             rootMargin: '0px 0px -40%',
         });
-    };
+    }
 
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
